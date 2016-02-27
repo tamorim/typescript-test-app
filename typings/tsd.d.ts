f2a0be9e60cca9f72e50e6d95814514e3670ef0c
Thor Amorim <thoramorim@gmail.com>
2016-02-27 05:22:02 -0300

    Remove typings from tracking

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="react/react.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react-redux/react-redux.d.ts" />
-/// <reference path="redux/redux.d.ts" />
-/// <reference path="redux-devtools/redux-devtools.d.ts" />
